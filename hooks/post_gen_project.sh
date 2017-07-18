1539c6e7055797e1cc06f6fee42dcc59498090b6
Edward Valentini <edward@interlook.com>
2017-07-18 16:02:25 -0700

    fix test file

--- hooks/post_gen_project.sh
+++ hooks/post_gen_project.sh
@@ -4,4 +4,5 @@ set -e
 
 git init
 `pwd`/doupdateios
+git add .
 git commit -m "Initial Commit"
--- {{ cookiecutter.name }}/{{ cookiecutter.testname }}/{{ cookiecutter.testname }}.swift
+++ {{ cookiecutter.name }}/{{ cookiecutter.testname }}/{{ cookiecutter.testname }}.swift
@@ -6,31 +6,18 @@
 //  Copyright © 2017 {{ cookiecutter.organization_name }}. All rights reserved.
 //
 
-import XCTest
-@testable import CookieCutterName
+import Quick
+import Nimble
+@testable import {{ cookiecutter.name }}
 
-class CookieCutterNameTests: XCTestCase {
+class {{ cookiecutter.testname }} : QuickSpec {
+    override func spec() {
+        super.spec()
 
-    override func setUp() {
-        super.setUp()
-        // Put setup code here. This method is called before the invocation of each test method in the class.
-    }
-
-    override func tearDown() {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
-        super.tearDown()
-    }
-
-    func testExample() {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-    }
-
-    func testPerformanceExample() {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
+        describe("first test") {
+            it("testing 1 = 1") {
+                expect(1).to(equal(1))
+            }
         }
     }
-
 }
